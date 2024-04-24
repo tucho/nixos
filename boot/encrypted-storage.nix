55cc68f805198cfc26afc6e164fc84d374587c4b
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-04-23 22:40:58 -0300

    Change the way TRIM requests are disabled for the SSD.
    Instead of disabling the TRIM service globally, disallow this
    operation for the specific device in use.

--- boot/encrypted-storage.nix
+++ boot/encrypted-storage.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.initrd.luks.devices.luks.device = "/dev/disk/by-label/luks";
-  services.fstrim.enable = false; # A luks encrypted ssd should not be trimmed, for security reasons.
+  boot.initrd.luks.devices.luks = {
+    device = "/dev/disk/by-label/luks";
+    allowDiscards = false;
+  };
 }
