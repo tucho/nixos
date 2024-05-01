557bfcd948fbff3aa0297d9ea2a4bb54fa6b97e3
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-05-01 16:20:28 -0300

    Add extra locale settings
    Set collate and ctype as "C" and measurement and paper as
    "pt_BR" (both UTF-8).

--- localization/default.nix
+++ localization/default.nix
@@ -1,6 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_COLLATE = "C.UTF-8";
+      LC_CTYPE = "C.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+    };
+  };
   time.timeZone = "America/Sao_Paulo";
 }
