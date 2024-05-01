adae3bd8ca7e471f6811fe0c8b267b63732c87cd
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-05-01 18:20:55 -0300

    Configure XDG user directories

--- home.nix
+++ home.nix
@@ -8,4 +8,19 @@
     packages = with pkgs; [];
     stateVersion = "24.05";
   };
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+
+    documents = "${config.home.homeDirectory}/documents";
+    download = "${config.home.homeDirectory}/download";
+    music = "${config.home.homeDirectory}/music";
+    pictures = "${config.home.homeDirectory}/pictures";
+    videos = "${config.home.homeDirectory}/videos";
+
+    desktop = "${config.home.homeDirectory}/.desktop";
+    publicShare = "${config.home.homeDirectory}/.public";
+    templates = "${config.home.homeDirectory}/.templates";
+  };
 }
