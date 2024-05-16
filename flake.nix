17a008807316a032b0308ce279a832098ab199c9
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-05-16 20:55:04 -0300

    Allow switching generations when changing home manager config
    See https://nix-community.github.io/home-manager/nixos-options.xhtml#nixos-opt-home-manager.backupFileExtension

--- flake.nix
+++ flake.nix
@@ -27,6 +27,7 @@
 
         home-manager.nixosModules.home-manager {
           home-manager = {
+            backupFileExtension = "backup";
             useGlobalPkgs = true;
             useUserPackages = true;
             users.marcel = import ./home.nix;
