9e1f3c50b7d2d6dc75867205677ff7903ccb3fce
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-05-25 13:58:28 -0300

    Move git user configuration from home.nix to packages/git.nix

--- home.nix
+++ home.nix
@@ -50,68 +50,6 @@
   };
 
   programs = {
-    git = {
-      enable = true;
-
-      userName = "Marcel Campello";
-      userEmail = "marcel.campello@prafrentex.com.br";
-
-      aliases = {
-        logg = "log --graph --pretty=format:'%C(yellow)%h%C(reset) %C(cyan)%ai%C(reset) %C(auto)%d%C(reset) %C(green)%an%C(reset) %C(magenta)%G?%C(reset) %s'";
-      };
-
-      extraConfig = {
-        core = {
-          whitespace = "warn,tab-in-indent";
-          editor = "emacsclient -nw";
-        };
-        init = {
-          defaultBranch = "main";
-        };
-        status = {
-          showUntrackedFiles = "all";
-        };
-        commit = {
-          gpgSign = true;
-        };
-        pull = {
-          ff = "only";
-        };
-        push = {
-          default = "simple";
-        };
-        merge = {
-          conflictStyle = "diff3";
-        };
-        diff = {
-          algorithm = "histogram";
-        };
-        submodule = {
-          recurse = true;
-        };
-        color = {
-          ui = "auto";
-        };
-        "color \"branch\"" = {
-          current = "yellow reverse";
-          local = "yellow";
-          remote = "green";
-        };
-        "color \"diff\"" = {
-          meta = "yellow bold";
-          frag = "magenta bold";
-          old = "red bold";
-          new = "green bold";
-          whitespace = "red reverse";
-        };
-        "color \"status\"" = {
-          added = "yellow";
-          changed = "green";
-          untracked = "cyan";
-        };
-      };
-    };
-
     gpg = {
       enable = true;
       homedir = "${config.xdg.dataHome}/gnupg";
