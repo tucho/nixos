e7d6115c962ec2c7df08de65632ef4a567181d85
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-06-25 16:36:55 -0300

    Fix the path for the user's downloads directory
    While the option name is singular (download), the default for the
    actual directory is plural (downloads).

--- users/marcel.nix
+++ users/marcel.nix
@@ -29,7 +29,7 @@
         createDirectories = true;
 
         documents = "${config.home-manager.users.marcel.home.homeDirectory}/documents";
-        download = "${config.home-manager.users.marcel.home.homeDirectory}/download";
+        download = "${config.home-manager.users.marcel.home.homeDirectory}/downloads";
         music = "${config.home-manager.users.marcel.home.homeDirectory}/music";
         pictures = "${config.home-manager.users.marcel.home.homeDirectory}/pictures";
         videos = "${config.home-manager.users.marcel.home.homeDirectory}/videos";
