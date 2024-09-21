4bcd02f085df19513069248dff421c2518f98af1
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-09-21 19:43:12 -0300

    Set shortcuts options

--- kde/plasma-manager.nix
+++ kde/plasma-manager.nix
@@ -164,6 +164,71 @@
           };
         };
       };
+      
+      shortcuts = {
+        kmix = {
+          increase_volume = "Volume Up";
+          decrease_volume = "Volume Down";
+          mute = "Volume Mute";
+          increase_volume_small = "Shift+Volume Up";
+          decrease_volume_small = "Shift+Volume Down";
+          increase_microphone_volume = "Alt+Volume Up";
+          decrease_microphone_volume = "Alt+Volume Down";
+          mic_mute = "Alt+Volume Mute";
+        };
+
+        kwin = {
+          "Switch to Desktop 1" = "Meta+1";
+          "Switch to Desktop 2" = "Meta+2";
+          "Switch to Desktop 3" = "Meta+3";
+          "Switch to Desktop 4" = "Meta+4";
+          
+          "Walk Through Windows" = "Alt+Tab";
+          "Walk Through Windows (Reverse)" = "Alt+Shift+Tab";
+          
+          "Window to Desktop 1" = "Meta+!";
+          "Window to Desktop 2" = "Meta+@";
+          "Window to Desktop 3" = "Meta+#";
+          "Window to Desktop 4" = "Meta+$";
+          
+          "Window Close" = "Meta+Shift+Q";
+          "Window Maximize" = "Meta+PgUp";
+          "Window Minimize" = "Meta+PgDown";
+          
+          "Window Quick Tile Top" = "Meta+Up";
+          "Window Quick Tile Bottom" = "Meta+Down";
+          "Window Quick Tile Left" = "Meta+Left";
+          "Window Quick Tile Right" = "Meta+Right";
+          
+          view_zoom_in = "Meta++";
+          view_zoom_out = "Meta+-";
+          view_actual_size = "Meta+0";
+        };
+
+        org_kde_powerdevil = {
+          "Increase Screen Brightness" = "Monitor Brightness Up";
+          "Decrease Screen Brightness" = "Monitor Brightness Down";
+          "Increase Screen Brightness Small" = "Shift+Monitor Brightness Up";
+          "Decrease Screen Brightness Small" = "Shift+Monitor Brightness Down";
+          
+          "Increase Keyboard Brightness" = "Keyboard Brightness Up";
+          "Decrease Keyboard Brightness" = "Keyboard Brightness Down";
+          "Toggle Keyboard Backlight" = "Keyboard Light On/Off";
+          
+          powerProfile = "Meta+B";
+        };
+
+        plasmashell = {
+          "activate application launcher" = "Meta";
+        };
+
+        "services/emacs.desktop"."_launch" = "Meta+Shift+E";
+        "services/emacsclient.desktop"."_launch" = "Meta+E";
+        "services/org.kde.dolphin.desktop"."_launch" = "Meta+D";
+        "services/org.kde.kcalc.desktop"."_launch" = "Meta+C";
+        "services/systemsettings.desktop"."_launch" = "Meta+S";
+        yakuake.toggle-window-state = "Meta+Return";
+      };
     };
   };
 }
