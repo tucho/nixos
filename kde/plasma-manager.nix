2268ea146c8a50afaf1ebc3275d6c92a1a26bff5
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-09-21 19:40:08 -0300

    Set fonts options

--- kde/plasma-manager.nix
+++ kde/plasma-manager.nix
@@ -12,6 +12,38 @@
         soundTheme = "ocean";
         wallpaper = "${config.home-manager.users.marcel.xdg.userDirs.pictures}/wallpapers/millennium-falcon.gif";
       };
+      
+      fonts = {
+        general = {
+          family = "Noto Sans";
+          pointSize = 10;
+        };
+
+        fixedWidth = {
+          family = "Hurmit Nerd Font";
+          pointSize = 10;
+        };
+
+        small = {
+          family = "Noto Sans";
+          pointSize = 8;
+        };
+
+        toolbar = {
+          family = "Noto Sans";
+          pointSize = 10;
+        };
+
+        menu = {
+          family = "Noto Sans";
+          pointSize = 10;
+        };
+
+        windowTitle = {
+          family = "Noto Sans";
+          pointSize = 10;
+        };
+      };
     };
   };
 }
