986c49911c3b2bf4b7c14430a59159698730aaed
Marcel Campello <marcel.campello@prafrentex.com.br>
2025-01-10 02:22:05 -0300

    Enable nvidia's offload mode

--- hardware/gpu-nvidia.nix
+++ hardware/gpu-nvidia.nix
@@ -13,6 +13,10 @@
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
         reverseSync.enable = true;
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
       };
     };
     graphics = {
