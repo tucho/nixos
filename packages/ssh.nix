36c95e6d823595bd224eca2c0926eb2a143c030d
Marcel Campello <marcel.campello@prafrentex.com.br>
2025-10-12 16:57:48 -0300

    Disable ssh forwardAgent and defaultConfig
    Replicate the default config into the match block for "*", now setting
    forwardAgent to false.

--- packages/ssh.nix
+++ packages/ssh.nix
@@ -5,13 +5,22 @@
     programs = {
       ssh = {
         enable = true;
-        forwardAgent = true;
-
+        enableDefaultConfig = false;
         matchBlocks = {
           "*" = {
             match = ''
               host * exec "gpg-connect-agent UPDATESTARTUPTTY /bye"
             '';
+            forwardAgent = false;
+            addKeysToAgent = "no";
+            compression = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            hashKnownHosts = false;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
           };
         };
       };
