4bcb43438f52d66c958ad369fff340a5b55c0709
Marcel Campello <marcel.campello@prafrentex.com.br>
2025-10-12 17:45:25 -0300

    Install hyprland
    Configure hyprland globally setting its xdg-desktop-portal
    implementation and enabling uwsm. In the home manager scope, disable
    the systemd option (to prevent conflicts with the global uwsm option)
    and set a bare minimal configuration, with monitor settings and a
    small set of bindings. Also install kitty.

---
+++ hyprland/default.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.hyprland = {
+    enable = true;
+    package = pkgs.hyprland;
+    portalPackage = pkgs.xdg-desktop-portal-hyprland;
+    withUWSM = true;
+  };
+
+  home-manager.users.marcel = {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      systemd.enable = false;
+      settings = {
+        monitor = "eDP-1, 1920x1080, 0x0, 1";
+        bind =
+          [
+            "SUPER, RETURN, exec, kitty"
+            "SUPER, E, exec, emacsclient -nc"
+            "SUPER, F, exec, firefox"
+            "SUPER SHIFT, Q, killactive"
+            "SUPER SHIFT, END, exit"
+          ];
+      };
+    };
+
+    programs.kitty.enable = true;
+  };
+}
