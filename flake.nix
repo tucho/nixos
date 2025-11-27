90f28cc0b742efa01571162becf45f1c5b07b8ec
Marcel Campello <marcel.campello@prafrentex.com.br>
2025-11-27 17:14:38 -0300

    Remove the branch from home manager repo URL

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
