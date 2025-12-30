b5fc14314f3279d07a042bd3dc43004dc8421a9a
Marcel Campello <marcel.campello@prafrentex.com.br>
2025-12-30 17:42:21 -0300

    Install audiocd-kio and k3b to use my new CD drive.

--- kde/applications.nix
+++ kde/applications.nix
@@ -2,9 +2,13 @@
 
 {
   environment.systemPackages = with pkgs.kdePackages; [
+    audiocd-kio
+    dolphin
     dragon
     filelight
+    k3b
     kcalc
+    kio
     krohnkite
     ktorrent
     skanpage
