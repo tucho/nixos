5d118735b8252603c105cb714fb55f5cdc2ec867
Marcel Campello <marcel.campello@prafrentex.com.br>
2026-02-06 18:36:47 -0300

    Create keybindings to move focus, windows and workspaces

--- hyprland/default.nix
+++ hyprland/default.nix
@@ -12,6 +12,7 @@
       systemd.enable = false;
       settings = {
         monitor = "eDP-1, 1920x1080, 0x0, 1";
+
         bind =
           [
             "SUPER, RETURN, exec, kitty"
@@ -19,10 +20,48 @@
             "SUPER, F, exec, firefox"
             "SUPER SHIFT, Q, killactive"
             "SUPER SHIFT, END, exit"
+
+            # Move focus with SUPER + arrow keys
+            "SUPER, LEFT, movefocus, l"
+            "SUPER, RIGHT, movefocus, r"
+            "SUPER, UP, movefocus, u"
+            "SUPER, DOWN, movefocus, d"
+
+            # Move window with SUPER + SHIFT + arrow keys
+            "SUPER SHIFT, LEFT, movewindow, l"
+            "SUPER SHIFT, RIGHT, movewindow, r"
+            "SUPER SHIFT, UP, movewindow, u"
+            "SUPER SHIFT, DOWN, movewindow, d"
+
+            # Switch workspaces with SUPER + [0-9]
+            "SUPER, 1, workspace, 1"
+            "SUPER, 2, workspace, 2"
+            "SUPER, 3, workspace, 3"
+            "SUPER, 4, workspace, 4"
+            "SUPER, 5, workspace, 5"
+            "SUPER, 6, workspace, 6"
+            "SUPER, 7, workspace, 7"
+            "SUPER, 8, workspace, 8"
+            "SUPER, 9, workspace, 9"
+            "SUPER, 0, workspace, 10"
+
+            # Move active window to a workspace with SUPER + SHIFT + [0-9]
+            "SUPER SHIFT, 1, movetoworkspace, 1"
+            "SUPER SHIFT, 2, movetoworkspace, 2"
+            "SUPER SHIFT, 3, movetoworkspace, 3"
+            "SUPER SHIFT, 4, movetoworkspace, 4"
+            "SUPER SHIFT, 5, movetoworkspace, 5"
+            "SUPER SHIFT, 6, movetoworkspace, 6"
+            "SUPER SHIFT, 7, movetoworkspace, 7"
+            "SUPER SHIFT, 8, movetoworkspace, 8"
+            "SUPER SHIFT, 9, movetoworkspace, 9"
+            "SUPER SHIFT, 0, movetoworkspace, 10"
           ];
+
         general = {
           layout = "master";
         };
+
         master = {
           new_status = "slave";
         };
