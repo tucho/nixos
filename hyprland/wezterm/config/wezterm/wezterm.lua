3256b7c1316159e2565224687a14ecffaedaa424
Marcel Campello <marcel.campello@prafrentex.com.br>
2026-03-16 14:28:56 -0300

    Rewrite wezterm config file to use config_builder function

--- hyprland/wezterm/config/wezterm/wezterm.lua
+++ hyprland/wezterm/config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 
-return {
-  hide_tab_bar_if_only_one_tab = true
-}
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
