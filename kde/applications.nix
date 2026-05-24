ea9b510f50bae27ccc008b6bb4c4a56d92efb9f8
Marcel Campello <marcel.campello@prafrentex.com.br>
2026-05-23 23:21:30 -0300

    Install geometry change (plasma desktop effect)

--- kde/applications.nix
+++ kde/applications.nix
@@ -1,16 +1,17 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs.kdePackages; [
-    dolphin
-    dragon
-    filelight
-    kcalc
-    kio
-    kio-extras
-    krohnkite
-    ktorrent
-    skanpage
-    xdg-desktop-portal-kde
+  environment.systemPackages = with pkgs; [
+    kdePackages.dolphin
+    kdePackages.dragon
+    kdePackages.filelight
+    kdePackages.kcalc
+    kdePackages.kio
+    kdePackages.kio-extras
+    kdePackages.krohnkite
+    kdePackages.ktorrent
+    kdePackages.skanpage
+    kdePackages.xdg-desktop-portal-kde
+    kwin-script-geometry-change
   ];
 }
