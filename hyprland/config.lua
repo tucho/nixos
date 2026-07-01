8ac561b47dc83d64591534f4210c8ca03bbd140e
Marcel Campello <marcel.campello@prafrentex.com.br>
2026-07-01 10:41:19 -0300

    Configure smart gaps

--- hyprland/config.lua
+++ hyprland/config.lua
@@ -102,3 +102,21 @@ hl.config({
         dim_strength = 0.25,
     },
 })
+
+-- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+-- "Smart gaps" / "No gaps when only"
+-- uncomment all if you wish to use that.
+hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
+hl.window_rule({
+    name  = "no-gaps-wtv1",
+    match = { float = false, workspace = "w[tv1]" },
+    border_size = 0,
+    rounding    = 0,
+})
+hl.window_rule({
+    name  = "no-gaps-f1",
+    match = { float = false, workspace = "f[1]" },
+    border_size = 0,
+    rounding    = 0,
+})
